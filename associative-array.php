5d5622944e2b0405ef4e431d595026450d76af6e
sachico-bull <135416262+sachico-bull@users.noreply.github.com>
2023-10-03 08:35:41 +0900

    課題007提出いたします
    課題007提出いたします。
    どうぞよろしくお願いいたします。

---
+++ associative-array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang ="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>kadai_007</title>
+</head>
+
+<body>
+<p>
+<?php
+
+ $personal_data = ['name' => 'onion', 'price' => 200 ,'weight' => 160 ];
+
+ print_r($personal_data);
+
+ ?>
+ </p>
+
+</bpdy>
+
+</html>
+
